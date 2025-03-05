f2b83c84f3b5b1bebec729a503a92077470d9bf2
hacksecureofficial <hacksecure.info@gmail.com>
2025-03-06 02:37:06 +0530

    page.js

--- app/blog/page.js
+++ app/blog/page.js
@@ -10,7 +10,7 @@ export default function BlogPage() {
       title: "How to Learn Cybersecurity",
       image: "/hacksec.jpeg",
       description: "A beginner's guide to understanding and learning cybersecurity concepts. Start your journey into the world of hacking and security.",
-      link: "/readmore1.js"
+      link: "app/blog/readmore1.js"
     },
     {
       title: "Understanding Ethical Hacking",
