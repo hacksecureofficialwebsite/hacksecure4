202802001fcaee1e8678692edf01d6646e4c2c9e
hacksecureofficial <hacksecure.info@gmail.com>
2025-03-17 23:47:55 +0530

    TrustedBy.js

--- app/components/TrustedBy.js
+++ app/components/TrustedBy.js
@@ -33,7 +33,7 @@ export default function TrustedBy() {
     return () => clearInterval(scrollInterval)
   }, [])
 
-  const Trusted & Recognized On= [
+  const TrustedRecognized = [
     { name: "Company 1", logo: "/placeholder.svg" },
     { name: "Company 2", logo: "/placeholder.svg" },
     { name: "Company 3", logo: "/placeholder.svg" },
