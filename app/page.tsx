a84836ab21d32493a4ef5728078dff716e385465
hacksecureofficialwebsite <website.hacksecure@gmail.com>
2025-04-06 20:33:47 +0530

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,18 +17,3 @@ export default function Home() {
     </main>
   )
 }
-
-
-import { Analytics } from '@vercel/analytics/react';
-
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Analytics />
-    </>
-  );
-}
-
-export default MyApp;
-
